0d4dae25d0ef92c191287976d7d556090f86a3a5
Bhavith Manapoty <catchbhav2002@gmail.com>
2025-07-01 19:34:22 -0400

    first commit

---
+++ postcss.config.js
@@ -0,0 +1,6 @@
+export default {
+  plugins: {
+    '@tailwindcss/postcss': {}, // use the v4 PostCSS plugin :contentReference[oaicite:8]{index=8}
+    autoprefixer: {}, // vendor prefixes for cross-browser support
+  },
+};
